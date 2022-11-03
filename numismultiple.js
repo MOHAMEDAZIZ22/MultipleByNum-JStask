5190d4878f092bdd1a8ac720796b30099839e85f
Mohamed Aziz <103256304+MOHAMEDAZIZ22@users.noreply.github.com>
2022-11-03 22:02:18 +0530

    Add files via upload

---
+++ numismultiple.js
@@ -0,0 +1,11 @@
+let a = 12
+
+if (a == 0) {
+    console.log("input is 0")
+}
+else if (a % 7 == 0) {
+    console.log("is number multiple by 7");
+}
+else {
+    console.log("is number not multiple by 7");
+}
